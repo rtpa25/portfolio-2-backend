9fedc9369e385c21488e36acab2daf40056e5966
Ronit Panda <pandaronit25@gmail.com>
2021-12-06 21:39:16 +0530

    deploy

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,10 @@ app.use(cookieParser());
 app.use(express.json({ limit: '50MB' }));
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://portfolio-2-frontend.vercel.app',
+    ],
     credentials: true,
   })
 );
